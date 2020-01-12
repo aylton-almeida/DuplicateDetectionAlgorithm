1f7147b983912e03763f81d7bb7c61d3991748b9
Aylton Almeida <38439416+stevreeper@users.noreply.github.com>
2020-01-12 16:50:10 +0100

    Update MongoDBCreadentials.py

--- scripts/api/MongoDBCreadentials.py
+++ scripts/api/MongoDBCreadentials.py
@@ -1,2 +1,12 @@
-# Insert your password for the admin user here
-mongo_pass = "******"
+# Insert your credentials for the atlas mongo db
+mongo_pass = "<YOUR_PASSWORD>"
+mongo_url = "<YOUR_URL>"
+mongo_database = "<YOUR_DATABASE>"
+mongo_collection = "<YOUR_COLLECTION>"
+
+mongo_credentials = {
+    'connection': mongo_url.format(mongo_pass),
+    'database': mongo_database,
+    'collection': mongo_collection
+}
+
